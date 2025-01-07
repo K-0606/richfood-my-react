ce6dcb0bf19859b8070c8ca06b95797a41a48ec3
jiajia <k22861310@gmail.com>
2025-01-07 11:05:52 +0800

    合併修改

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,18 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import LoginPage from './pages/LoginPage'
 import './App.css';
+import Header from "./components/layout/Header"
+
 
 
 function App() {
-  
-
   return (
     <>
       <h1>Hello RichFood</h1>
       <LoginPage/>
+      <Header/>
     </>
   )
 }
 
-export default App
+
