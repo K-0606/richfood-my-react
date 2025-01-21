1270a35d22f405aa541636f6a9ca00e7438fb29a
djspchen <djspchen@gmail.com>
2025-01-21 16:01:11 +0800

    couponStoreOrder

--- src/pages/StoreComponents/StoreCoupons.jsx
+++ src/pages/StoreComponents/StoreCoupons.jsx
@@ -1,11 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Box, Button, Grid, Typography } from '@mui/material';
 
 const StoreCoupons = () => {
   const [coupons, setCoupons] = useState([
-    { id: 1, code: 'ABC123', status: '未使用' },
-    { id: 2, code: 'DEF456', status: '未使用' },
+    // { id: 1, code: 'ABC123', status: '未使用' },
+    // { id: 2, code: 'DEF456', status: '未使用' },
   ]);
+
+  useEffect(()=>{
+      const fetchCoupons=async ()=>{
+        try{
+          const response=await fetch('http://localhost:8080/couponsOrder/selectAllStoreCouponsOrder',
+          { 
+            method: 'GET',
+            credentials: 'include', // 如果需要攜帶憑據（如 Cookies）
+            headers: {
+              'Content-Type': 'application/json',
+            }
+        });
+          const data=await response.json();
+          console.log(data);
+  
+          // 映射資料結構
+          const formattedData=data.map((item)=>({
+            id:item.orderId,
+            code: item.orderId,
+            status: item.status ? '未使用' : '已使用',
+          })) 
+  
+          setCoupons(formattedData);
+          console.log(formattedData)
+        }catch(error){
+          console.error(error);
+        }
+      }
+      fetchCoupons();
+      
+  
+    },[]);
 
   const handleScanSuccess = (scanResult) => {
     // 根據掃描結果更新餐券的狀態
