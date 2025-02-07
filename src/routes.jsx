c0560bb52ced51b607084b2cb2bf97695416c5c0
k <kunkun7865@gmail.com>
2025-02-07 16:29:35 +0800

    fix

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,3 +1,50 @@
+// src/routes.js
+import Home from './pages/Home';
+import LoginPage from './pages/LoginPage';
+import MemberRegister from './pages/MemberRegister';
+import MemberLoginPage from './pages/MemberLoginPage';
+import SystemManage from './pages/SystemManage';
+import BookPage from './pages/BookPage';
+import ContactUsPage from './pages/ContactUsPage';
+import PaymentPage from './pages/PaymentPage';
+import ErrorPage from './pages/ErrorPage';
+import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
+import HomePicture1 from './pages/HomeAll/HomePicture1';
+import HomePicture2 from './pages/HomeAll/HomePicture2';
+import HomePicture3 from './pages/HomeAll/HomePicture3';
+import HomeHeroImage from './pages/HomeAll/HomeHeroImage';
+import SearchStore from './pages/SearchStore';
+import MemberProfile from './pages/MemberComponents/MemberProfile';
+import Comments from './pages/MemberComponents/Comments';
+import Collections from './pages/MemberComponents/Collections';
+import Coupons from './pages/MemberComponents/Coupons';
+import Reservations from './pages/MemberComponents/Reservations';
+import MyRecommend from './pages/MyRecommend';
+import StorePage from './pages/StorePage';
+import PopularPage from './pages/PopularPage';
+import StoreLoginPage from './pages/StoreLoginPage';
+import StoreQRCodeScanner from './pages/StoreComponents/StoreQRCodeScanner';
+import Test2 from './pages/SearchStoreAll/Test2';
+import Test3 from './pages/Test3';
+import Test4 from './pages/Test4';
+import UseCoupon from './pages/MemberComponents/UseCoupon';
+// 英文版
+import HomeEnglish from './pages/HomeEnglish';
+import HomeName1E from './pages/HomeAllEnglish/HomeName1E';
+import HomeName2E from './pages/HomeAllEnglish/HomeName2E';
+import HomeName3E from './pages/HomeAllEnglish/HomeName3E';
+import HomePicture1E from './pages/HomeAllEnglish/HomePicture1E';
+import HomePicture2E from './pages/HomeAllEnglish/HomePicture2E';
+import HomePicture3E from './pages/HomeAllEnglish/HomePicture3E';
+import HomeHeroImageE from './pages/HomeAllEnglish/HomeHeroImageE';
+import ForgotPasswordPage from './pages/ForgotPasswordPage';
+import ResetPasswordPage from './pages/ResetPasswordPage';
+import RestaurantDetail from './pages/Restaurant/RestaurantDetail';
+import MySearchField from './pages/MySearch/MySearchField';
+
+
+// 定義路由配置
 const routes = [
   { path: '/richfood-my-react/', element: <Home /> },
   { path: '/richfood-my-react/login', element: <LoginPage /> },
@@ -15,32 +62,46 @@ const routes = [
   { path: '/richfood-my-react/forgot-password', element: <ForgotPasswordPage /> },
   { path: '/richfood-my-react/reset-password', element: <ResetPasswordPage /> },
   { path: '/richfood-my-react/restaurant/:id', element: <RestaurantDetail /> },
-  { path: '/richfood-my-react/search', element: <MySearchField /> },
-  { path: '/richfood-my-react/profile', element: <MemberProfile /> },
-  { path: '/richfood-my-react/comments', element: <Comments /> },
-  { path: '/richfood-my-react/collections', element: <Collections /> },
-  { path: '/richfood-my-react/coupons', element: <Coupons /> },
-  { path: '/richfood-my-react/reservations', element: <Reservations /> },
-  { path: '/richfood-my-react/use-coupon/:couponId', element: <UseCoupon /> },
-  { path: '/richfood-my-react/Header', element: <Header /> },
-  { path: '/richfood-my-react/Footer', element: <Footer /> },
-  { path: '/richfood-my-react/HomePicture1', element: <HomePicture1 /> },
-  { path: '/richfood-my-react/HomePicture2', element: <HomePicture2 /> },
-  { path: '/richfood-my-react/HomePicture3', element: <HomePicture3 /> },
-  { path: '/richfood-my-react/HomeHeroImage', element: <HomeHeroImage /> },
-  { path: '/richfood-my-react/SearchStore', element: <SearchStore /> },
-  { path: '/richfood-my-react/StorePage', element: <StorePage /> },
-  { path: '/richfood-my-react/Test2', element: <Test2 /> },
-  { path: '/richfood-my-react/Test3', element: <Test3 /> },
-  { path: '/richfood-my-react/Test4', element: <Test4 /> },
+  
+
+  //餐廳搜尋 routes
+  // { path: "/search/:region?/:type?", element: <MySearchField /> },
+  // { path: "/search/:type", element: <MySearchField /> },
+  // { path: "/search/:region", element: <MySearchField /> },
+  
+  { path: "/search", element: <MySearchField /> },
+
+  // Members-related routes
+  { path: '/profile', element: <MemberProfile /> },
+  { path: '/comments', element: <Comments /> },
+  { path: '/collections', element: <Collections /> },
+  { path: '/coupons', element: <Coupons /> },
+  { path: '/reservations', element: <Reservations /> },
+  { path: '/use-coupon/:couponId', element: <UseCoupon /> },
+  
+  // Miscellaneous
+  { path: '/Header', element: <Header /> },
+  { path: '/Footer', element: <Footer /> },
+  { path: '/HomePicture1', element: <HomePicture1 /> },
+  { path: '/HomePicture2', element: <HomePicture2 /> },
+  { path: '/HomePicture3', element: <HomePicture3 /> },
+  { path: '/HomeHeroImage', element: <HomeHeroImage /> },
+  { path: '/SearchStore', element: <SearchStore /> },
+  { path: '/StorePage', element: <StorePage /> },
+  { path: '/Test2', element: <Test2 /> },
+  { path: '/Test3', element: <Test3 /> },
+  { path: '/Test4', element: <Test4 /> },
 
   // 英文版  
-  { path: "/richfood-my-react/HomeEnglish", element: <HomeEnglish /> },
-  { path: "/richfood-my-react/HomeName1E", element: <HomeName1E /> },
-  { path: "/richfood-my-react/HomeName2E", element: <HomeName2E /> },
-  { path: "/richfood-my-react/HomeName3E", element: <HomeName3E /> },
-  { path: "/richfood-my-react/HomePicture1E", element: <HomePicture1E /> },
-  { path: "/richfood-my-react/HomePicture2E", element: <HomePicture2E /> },
-  { path: "/richfood-my-react/HomePicture3E", element: <HomePicture3E /> },
-  { path: "/richfood-my-react/HomeHeroImageE", element: <HomeHeroImageE /> }
+  { path: "/HomeEnglish", element: <HomeEnglish /> }, 
+  { path: "/HomeName1E", element: <HomeName1E /> }, 
+  { path: "/HomeName2E", element: <HomeName2E /> }, 
+  { path: "/HomeName3E", element: <HomeName3E /> }, 
+  { path: "/HomePicture1E", element: <HomePicture1E /> }, 
+  { path: "/HomePicture2E", element: <HomePicture2E /> }, 
+  { path: "/HomePicture3E", element: <HomePicture3E /> }, 
+  { path: "/HomeHeroImageE", element: <HomeHeroImageE /> }
+
 ];
+
+export default routes;
