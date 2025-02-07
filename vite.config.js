22bca429954d2fb00eeaa1041c4ec6d8ed6dc783
k <kunkun7865@gmail.com>
2025-02-07 14:55:08 +0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/richfood-my-react/', 
+  build: {
+    outDir: 'dist', // 確保構建輸出到 'dist' 文件夾
+  },
   plugins: [react()],
 })
