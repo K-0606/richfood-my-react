b8c565727cd707985fd8e2444561780df13bd1a6
k <kunkun7865@gmail.com>
2025-02-08 11:00:45 +0800

    fix again

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
       <ThemeProvider theme={theme}>
         <AvatarProvider>
           {/* 這裡只使用 RouterProvider 並設置 basename */}
-          <RouterProvider router={router} basename="/richfood-my-react" />
+          <RouterProvider router={router}  />
         </AvatarProvider>
       </ThemeProvider>
     </UserProvider>
